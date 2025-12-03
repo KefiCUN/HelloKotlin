01f6174e889ef0fbc3f3f14de68a4c7c233465a1
KefiCUN <melmanref@gmail.com>
2025-12-03 12:20:16 +0300

    +sixty_class\IfElse.kt learn if..else, works

--- sixty_class/IfElse.kt
+++ sixty_class/IfElse.kt
@@ -24,6 +24,7 @@
 // Итоговая оценка: комбинация экзамена + ДЗ + активность
 // Повышение оценки: за дополнительные работы
 // Особые случаи: автопроход, пересдача
+
 import java.io.BufferedReader
 import java.io.InputStreamReader
 
@@ -47,20 +48,27 @@ fun main(args: Array<String>) {
         print("Были дополнительные работы? (да/нет): ")
         var thereAdditionalJobs:String =  readLineWithEncoding()?.trim()?.lowercase() ?: "нет"
         println("=== РЕЗУЛЬТАТЫ ===")
-        if ((attendance >= 70.toUByte()) and (averageHomework >= 80.toUByte())){
+        if ((attendance >= 70.toUByte()) and (averageHomework >= 80.toUByte()) and (assessment >= 50.toUByte())){
             
             println("Допуск к экзамену: ✅ ДОПУЩЕН")
             var resultAssessment = if((assessment >= 79.toUByte()) and (averageHomework >= 94.toUByte()) and (activity == 3.toUByte())) 5 else if((assessment >= 69.toUByte()) and (averageHomework >= 89.toUByte()) and (activity >= 2.toUByte())) 4 else 3
             var additionalPoints = if(thereAdditionalJobs == "да") 5 else 0
             var activityPoints = if(activity == 3.toUByte()) 5 else 0
-            var sumAssessment = assessment.toInt() + resultAssessment + additionalPoints +activityPoints
+            var sumAssessment = if ((activity == 3.toUByte()) and (thereAdditionalJobs == "да") and (averageHomework == 100.toUByte())) {println("Автомат"); 100} else (assessment.toInt() + resultAssessment + additionalPoints +activityPoints)
+            
             println("Балл за активность: $activityPoints")
             println("Дополнительные работы: $additionalPoints \n")
             println("Итоговый балл: $sumAssessment")
             var estimation = if(sumAssessment >= 94) 5 else if(sumAssessment >= 89) 4 else 3
             println("Оценка: $estimation")
+            
         }else{
             println("Допуск к экзамену: НЕ ДОПУЩЕН")
+            if ((attendance >= 70.toUByte()) and (averageHomework >= 80.toUByte())){
+                println("Пересдача доступна")
+            }else{
+                println("Пересдача не доступна")
+            }
         }
     }catch (e: Exception) {
         println("Ошибка: ${e.message}")
